fbd1f5365ea60b4b0b697f4cd537d2e2a499015f
yoshi0702 <grx12011@icloud.com>
2020-05-29 03:12:33 +0000

    topページレイアウト修正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,18 +1,6 @@
 class FavoritesController < ApplicationController
   before_action :authenticate_supporter!
 
-  def index
-  end
-
-  def show
-  end
-
-  def new
-  end
-
-  def edit
-  end
-
   def create
     @project = Project.find(params[:project_id])
     favorite = current_supporter.favorites.build(project_id: @project.id)
@@ -20,9 +8,6 @@ class FavoritesController < ApplicationController
     respond_to :js
   end
 
-  def update
-    
-  end
 
   def destroy
     @project = Project.find(params[:project_id])
