223d561d0321e2cae18519edf802f26b0e0f2e98
yoshi0702 <grx12011@icloud.com>
2020-06-15 15:10:45 +0000

    supporterマイページ　お気に入りプロジェクト追加

--- app/controllers/supporters_controller.rb
+++ app/controllers/supporters_controller.rb
@@ -4,17 +4,9 @@ class SupportersController < ApplicationController
   end
 
   def show
-      #supporter_project = SupportProject.where(supporter_id: current_supporter.id)
-      #@projects = Project.find(supporter_project.pluck(:project_id))
+      @favoriteprojects = current_supporter.favorites
       @projects = current_supporter.projects
-      #@remaining_day = DateTime.today -
-      #@remaining_day = (Date.new(@projects.created_at.utc.year, @projects.created_at.utc.month, @projects.created_at.utc.day) + @projects.post_period.to_i - Date.today).to_i
-      # @supporter_counts = {}
-      # @projects.each do |project|
-      #   supporter_count = SupportProject.where(project_id: project.id).group(:supporter_id).pluck(:supporter_id).count
-      #   @supporter_counts.store(project.id,supporter_count)
-      # end
-      # いいねした記事のみを表示するためにリンクのパラメータに[:favorite]を持たせる
+
   end
 
 
@@ -30,7 +22,7 @@ class SupportersController < ApplicationController
   end
 
   def update
-    @supporter = Supporter.find(current_supporter.id)
+      @supporter = Supporter.find(current_supporter.id)
         if @supporter.update(supporter_params)
            redirect_to supporter_path
         else
